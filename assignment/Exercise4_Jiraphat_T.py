a8310aa5447aeff15b8bcc149ccbb4c5abedbb07
JhopperJH <jumphopper.jh@gmail.com>
2021-06-21 16:20:37 +0700

    Create exe.4
    Just done the homework

---
+++ assignment/Exercise4_Jiraphat_T.py
@@ -0,0 +1,10 @@
+FEscore = 100.00
+GBscore = 87.50
+ICSscore = 96.75
+CPscore = 82.50
+
+print("--- Your Score ---")
+print("Foundation English : ", FEscore)
+print("General Business : ", GBscore)
+print("Introduction to Computer Systems : ", ICSscore)
+print("Computer Programming : ", CPscore)
