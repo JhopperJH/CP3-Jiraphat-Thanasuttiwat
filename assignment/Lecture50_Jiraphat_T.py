b68716e8ed4f953028b30a5552add6273ac8326f
JhopperJH <jumphopper.jh@gmail.com>
2021-07-28 19:18:33 +0700

    Create Lecture50_Jiraphat_T.py

---
+++ assignment/Lecture50_Jiraphat_T.py
@@ -0,0 +1,13 @@
+def plus(x,y):
+    print("ผลบวก = ",x+y)
+def minus(x,y):
+    print("ผลลบ = ",x-y)
+def multiplied(x,y):
+    print("ผลคูณ = ",x*y)
+def divide(x,y):
+    print("ผลหาร = ",x/y)
+
+plus(10,4)
+minus(10,4)
+multiplied(10,4)
+divide(10,4)
